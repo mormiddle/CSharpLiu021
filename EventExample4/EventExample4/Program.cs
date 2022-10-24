8fa4b0f7c627e4747ca2e00677867eed42fb101c
mormiddle <mormiddle@163.com>
2022-10-24 21:08:31 +0800

    三星事件调用

--- EventExample4/EventExample4/Program.cs
+++ EventExample4/EventExample4/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EventExample4
 {
@@ -10,6 +7,30 @@ namespace EventExample4
     {
         static void Main(string[] args)
         {
+            MyForm form = new MyForm();
+            form.ShowDialog();
+        }
+    }
+
+    class MyForm : Form
+    {
+        private TextBox textBox;
+        private Button button;
+
+        public MyForm()
+        {
+            this.textBox = new TextBox();
+            this.button = new Button();
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBox);
+            this.button.Click += this.ButtonClicked;
+            this.button.Text = "Say Hello";
+            this.button.Top = 100;
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            this.textBox.Text = "Hello, World!!!!!!!!!!!!!";
         }
     }
 }

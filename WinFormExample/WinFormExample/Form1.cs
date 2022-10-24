6b043b98bce17de2aa8a14a6ed67429f5468ea56
mormiddle <mormiddle@163.com>
2022-10-24 22:00:57 +0800

    可视化编程winform例子

---
+++ WinFormExample/WinFormExample/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.button3.Click += this.ButtonClicked;
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            if (sender == this.button1)
+            {
+                this.textBox1.Text = "Hello";
+            }
+            if (sender == this.button2)
+            {
+                this.textBox1.Text = "World";
+            }
+            if (sender == this.button3)
+            {
+                this.textBox1.Text = "Mr.Okey";
+            }
+        }
+    }
+}
